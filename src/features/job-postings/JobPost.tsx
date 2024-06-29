9db0889c781965b37c4e0c350f0a2bd36c3dadde
Spencer Chavez <sjchavez99@gmail.com>
2024-06-29 15:15:19 -0600

    updates

---
+++ src/features/job-postings/JobPost.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Card } from 'antd';
+import styled from 'styled-components';
+
+const JobPost = styled.div`
+    display: flex;
+    flex-direction: column;
+
+`;
+
+export default JobPost;
