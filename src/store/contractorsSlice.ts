8df48ca9a52fd0d6f23f4e856641c13db486e104
Spencer Chavez <sjchavez99@gmail.com>
2024-09-18 11:10:32 -0600

    updates

--- src/store/contractorsSlice.ts
+++ src/store/contractorsSlice.ts
@@ -15,7 +15,7 @@ const initialContractorProps: { [contractorId: number]: ContractorProps} = {};
 const initialContractorRatingsProps: { [contractorId: number]: ContractorRatingsProps} = {};
 for(let i=0; i<20; i++) {
     const exampleContractorProps: ContractorProps = {
-        name: 'Plumber Wannabe',
+        name: 'Plumber Joe',
         specialty: 'Master Plumber with 40 years of experience',
         jobCategories: ['Plumber', 'construction'],
         serviceArea: {
@@ -26,7 +26,7 @@ for(let i=0; i<20; i++) {
             radius: 100,
         },
         contractorId: i,
-        profilePicture: 'https://bloximages.newyork1.vip.townnews.com/stltoday.com/content/tncms/assets/v3/editorial/3/3b/33b9e530-5c2c-54ea-9de9-52ed38678502/55836d201d850.image.jpg',
+        profilePicture: 'https://www.shutterstock.com/image-photo/male-plumber-diagnoses-pipes-water-260nw-2358150009.jpg',
         portfolioImages: ['https://wentworthplumbing.ca/wp-content/uploads/2019/02/industrialplumbingpipes-800x450.jpg', 'https://trusteyman.com/wp-content/uploads/2020/07/commercial-plumber-1-1024x683.jpeg', 'https://i.redd.it/bj89ble82m5a1.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqYv6qIVUeE4xz4sSJfYTLLzy3BB_HHFegqA&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPbZtS_qJrmz1uh7evMzDLT700m1ZNt6YYcw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzKz21h6nx_xn_lq_zTTzrULJHgLM3M9H7IA&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTpy6un9wKIJCsuRaxrkpIJhGt_JXxT0-oqw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTpy6un9wKIJCsuRaxrkpIJhGt_JXxT0-oqw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp0KTZd0RnLjjTx9tOrGo1caUxjY9I-srVLQ&s'],
         services: ['Plumbing Repair', 'Plumbing Installation', 'Emergency Plumbing'],
         about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
