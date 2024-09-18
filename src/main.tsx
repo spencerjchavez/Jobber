37bccc609b1fc3557f8213e54726b536393e096e
Spencer Chavez <sjchavez99@gmail.com>
2024-09-18 09:17:10 -0600

    gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Jobber'>
       <Provider store={store}>
         <App />
       </Provider>
