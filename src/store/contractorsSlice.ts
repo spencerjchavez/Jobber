eef6c3717d58737a5574f9462e3c491638c359e1
Spencer Chavez <sjchavez99@gmail.com>
2024-09-18 11:14:12 -0600

    gh-pages

--- src/store/contractorsSlice.ts
+++ src/store/contractorsSlice.ts
@@ -34,7 +34,7 @@ for(let i=0; i<20; i++) {
 
     const ratings = Array.from({length: 10}).map(() => {
         return {
-            stars: Math.floor(Math.random() * 5) + 1,
+            stars: Math.floor(Math.random() * 3) + 3,
             text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.substring(0, Math.random()* 1200),
             contractorId: i,
             authorUserId: 1,
